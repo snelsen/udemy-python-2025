1c82a8749735a29cc301820e7f847ae519f9181e
Scotland Nelsen <snelsen@users.noreply.github.com>
2026-01-14 17:50:34 -0600

    Implemented removing completed todo items.

--- todo_app/web.py
+++ todo_app/web.py
@@ -13,11 +13,13 @@ st.title("My To-Do App")
 st.subheader("This is my to-do app.")
 st.write("This app is to improve your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Add a todo...",
               on_change=add_todo, key="new_todo")
-
-
-st.session_state
