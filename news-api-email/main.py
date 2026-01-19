6e98d361472ed12ab9f6abf6e1ca03c43a3b34d2
Scotland Nelsen <snelsen@users.noreply.github.com>
2026-01-19 16:12:03 -0600

    Modified news-api-email to add dynamic topic, links, subject, language, and urls.

--- news-api-email/main.py
+++ news-api-email/main.py
@@ -2,7 +2,15 @@ import requests
 from send_email import send_email
 
 api_key = "f5e9fc8a3fc243929738e3d8e2380b3c"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2025-12-19&sortBy=publishedAt&apiKey=f5e9fc8a3fc243929738e3d8e2380b3c"
+
+topic = "tesla"
+
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}" \
+      "&from=2025-12-19" \
+      "&sortBy=publishedAt" \
+      "&apiKey=f5e9fc8a3fc243929738e3d8e2380b3c" \
+      "&language=en"
 
 request = requests.get(url)
 
@@ -10,11 +18,13 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
-message = ""
-for article in content["articles"]:
+message = "Subject: Today's News\n"
+for article in content["articles"][:20]:
     message = message + article["title"] + "\n"
     if article["description"] is not None:
         message = message + article['description'] + "\n"
+    if article["url"] is not None:
+        message = message + article["url"] + "\n"
     message = message + "\n"
 
 message = message.encode("utf-8")
